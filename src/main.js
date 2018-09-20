bd5146b53702a10d9fc997018c44434794b88dfb
spemer <ghsspower@naver.com>
2018-09-21 00:30:21 +0900

    DEL remove store

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import 'babel-polyfill'
 import 'event-source-polyfill'
+import 'es6-promise/auto'
 
 import Vue from 'vue'
 import axios from 'axios'
 import App from '@/App.vue'
-import store from '@/store'
 import router from '@/routes'
 import VueI18n from 'vue-i18n'
 import VTooltip from 'v-tooltip'
@@ -50,7 +50,6 @@ window.i18n = new VueI18n({
 new Vue({
   el: '#app',
   i18n,
-  store,
   router,
   render: h => h(App)
 })
