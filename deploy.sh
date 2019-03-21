4d4f3e68d1c7aa545413dab54456a408118e8490
spemer <ghsspower@gmail.com>
2019-03-21 17:08:13 +0900

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,18 @@ echo "=============================="
 echo "${PWD##*/}"
 echo "=============================="
 
+# install dependencies
+npm install
+
 # run gulpfile.js image resizer
 trap 'echo Stop gulp-watch' SIGINT
 gulp watch
 trap SIGINT
 
+# serve with hot reload at localhost:8000
 npm run dev
 
-# build
+# build for production with minification
 npm run build
 
 # deploy with firebase cli
