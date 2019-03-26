7aa5a69e242c8f45e92544671f987f73b8a0e6e8
spemer <ghsspower@gmail.com>
2019-03-26 17:07:10 +0900

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -1,44 +1,153 @@
 #!/bin/bash
-echo "=============================="
-echo "${PWD##*/}"
-echo "=============================="
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
 
+echo "============================================================"
+echo "${BOLD}${PWD##*/}${RESET}"
+echo "============================================================"
+
+#============================================================
 # get OS type && install dependencies
-if [[ "$OSTYPE" == "win32" || "cygwin" || "msys" ]]; then
-  npm install --no-optional
-else
-  npm install
-fi
+#============================================================
+npm_install() {
+  while true; do
+    printf "\n"
+    read -p "${BOLD}Install dependencies? (Y/n)${RESET}" yn
+    case ${yn} in
+      [Yy]* )
+        if [[ "$OSTYPE" == "win32" || "cygwin" || "msys" ]]; then
+          npm install --no-optional
+        else
+          npm install
+        fi
+        break;;
 
+      [Nn]* ) return 0;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+}
+
+#============================================================
 # run gulpfile.js image resizer
-trap 'echo Stop gulp-watch' SIGINT
-sudo gulp watch
-trap SIGINT
+#============================================================
+image_resizer() {
+  while true; do
+    printf "\n"
+    read -p "${BOLD}Run gulpfile.js(image resizer)? (Y/n)${RESET}" yn
+    case ${yn} in
+      [Yy]* )
+        trap 'echo Stop gulp-watch' SIGINT
+        sudo gulp watch
+        trap SIGINT
+        break;;
 
+      [Nn]* ) return 0;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+}
+
+#============================================================
 # serve with hot reload at localhost:8000
-npm run dev
+#============================================================
+npm_run_dev() {
+  while true; do
+    printf "\n"
+    read -p "${BOLD}npm run dev? (Y/n)${RESET}" yn
+    case ${yn} in
+      [Yy]* ) npm run dev; break;;
+      [Nn]* ) return 0;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+}
 
+#============================================================
 # build for production with minification
-npm run build
+#============================================================
+npm_run_build() {
+  while true; do
+    printf "\n"
+    read -p "${BOLD}npm run build? (Y/n)${RESET}" yn
+    case ${yn} in
+      [Yy]* ) npm run build; break;;
+      [Nn]* ) return 0;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+}
 
+#============================================================
 # deploy with firebase cli
-printf "\n"
-firebase deploy
+#============================================================
+firebase_deploy() {
+  while true; do
+    printf "\n"
+    read -p "${BOLD}firebase deploy? (Y/n)${RESET}" yn
+    case ${yn} in
+      [Yy]* ) firebase deploy; break;;
+      [Nn]* ) return 0;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+}
 
-# get commit message
-printf "\n"
-IFS= read -r -p "Enter commit message: " commitmsg
+#============================================================
+# git commit
+#============================================================
+git_commit() {
+  while true; do
+    printf "\n"
+    read -p "${BOLD}git commit? (Y/n)${RESET}" yn
+    case ${yn} in
+      [Yy]* )
+        IFS= read -r -p "${BOLD}Enter commit message: ${RESET}" commitmsg
 
-# if commitmsg empty
-if [ -z "$commitmsg" ]
-then
-    echo "Commit message is empty"
-    commitmsg="Add files via upload"
-fi
+        # if commitmsg empty
+        if [ -z "$commitmsg" ]
+        then
+          echo "${BOLD}Commit message is empty${RESET}"
+          commitmsg="Add files via upload"
+        fi
 
-printf "\n"
-git add .
-git commit -m "$commitmsg"
-git push
+        printf "\n"
+        git add .
+        git commit -m "$commitmsg"
+        break;;
 
-exit
+      [Nn]* ) return 0;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+}
+
+#============================================================
+# git push
+#============================================================
+git_push() {
+  while true; do
+    printf "\n"
+    read -p "${BOLD}git push? (Y/n)${RESET}" yn
+    case ${yn} in
+      [Yy]* ) git push; break;;
+      [Nn]* ) return 0;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+}
+
+#============================================================
+# main
+#============================================================
+main() {
+  npm_install
+  image_resizer
+  npm_run_dev
+  npm_run_build
+  firebase_deploy
+  git_commit
+  git_push
+}
+
+main
